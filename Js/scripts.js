37c1b9149f202c57d34fc7774bf78b0bbd5f3043
Gregory Reyes <greyes3@udi.edu.co>
2023-06-27 22:39:45 -0500

    Implementación de la nueva función de carousel y de formulario calificame

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,6 +1,37 @@
-$(document).ready(function(){
+//corousel evento play y pause
+$(function() {
+  $('#carouselExampleDark').carousel({interval: 8000 });
+  $('#carouselButton').on("click",function(){
+      if($('#carouselButton').children('span').hasClass('bi-pause-fill')){
+         $('#carouselExampleDark').carousel('pause');
+         $('#carouselButton').children('span').removeClass('bi-pause-fill');
+         $('#carouselButton').children('span').addClass('bi-play-fill');
+      }
+      else if($('#carouselButton').children('span').hasClass('bi-play-fill')){
+         $('#carouselExampleDark').carousel('cycle');
+         $('#carouselButton').children('span').removeClass('bi-play-fill');
+         $('#carouselButton').children('span').addClass('bi-pause-fill');
+      }
+  });  
+});
+
+
+const form = document.querySelector("#formulario-calificame");
+const enviarFormulario = (e) => {
+  e.preventDefault();
+  
+  const { N_orden, servicio, sucursal} = e.target;
+    
+  console.log(N_orden.value, servicio.value, sucursal.value);
+
+};
+form.addEventListener("submit", enviarFormulario);
+
+
+
+/*$(document).ready(function(){
     $('#carouselExampleDark').carousel( { interval: 2000 } );
-    $('#carouselButton').click(function(){
+    $('#carouselButton').onClick("click", function(){
       if ($('carouselButton').children('span').hasClass('bi-pause-fill')){
         $('#carouselExampleDark').carousel('pause');
         $('#carouselButton').children('span').removeClass('bi-pause-fill');
@@ -13,12 +44,6 @@ $(document).ready(function(){
       }          
     }); 
     
-    $('#reserveButton').click(function(){
-        $('#reserveModal').modal('show');
-        }
-    );
-    $('#loginButton').click(function(){
-        $('#loginModal').modal('show');
-        }
-    );
   });
+*/
+ 
