09c3c7f53f338ea9593d4724f993a418817d40f7
Gregory Reyes <greyes3@udi.edu.co>
2023-07-02 18:18:00 -0500

    Implementación de js de calificación

--- Js/scripts.js
+++ Js/scripts.js
@@ -14,36 +14,40 @@ $(function() {
       }
   });  
 });
-
-
+//implementacion de la seccion calificame
 const form = document.querySelector("#formulario-calificame");
 const enviarFormulario = (e) => {
   e.preventDefault();
   
-  const { N_orden, servicio, sucursal} = e.target;
+  const { N_orden, servicio, sucursal, calificacion} = e.target;
     
-  console.log(N_orden.value, servicio.value, sucursal.value);
-
+  console.log(N_orden.value, servicio.value, sucursal.value, calificacion.value);
 };
+document.getElementById('formulario-calificame').reset();
+
+function registro() {
+  alert("Gracias por su calificación!");
+}
+
+/*
+function registro() {
+  if(document.getElementById("N_orden").value == ""){
+    alert("Por favor ingrese su número de orden");
+    
+  }
+  else if(document.getElementById("N_orden").value != ""){
+    alert("Gracias por su calificación!");
+  } 
+}
+*/
+
+document.addEventListener('DOMContentLoaded', function(){
+  let formulario = document.getElementById('formulario-calificame');
+  formulario.addEventListener('submit', function() {
+    formulario.reset();
+  });
+});
+
 form.addEventListener("submit", enviarFormulario);
 
-
-
-/*$(document).ready(function(){
-    $('#carouselExampleDark').carousel( { interval: 2000 } );
-    $('#carouselButton').onClick("click", function(){
-      if ($('carouselButton').children('span').hasClass('bi-pause-fill')){
-        $('#carouselExampleDark').carousel('pause');
-        $('#carouselButton').children('span').removeClass('bi-pause-fill');
-        $('#carouselButton').children('span').addClass('bi-play-fill');
-      }  
-      else if ($('carouselButton').children('span').hasClass('bi-play-fill')){
-        $('#carouselExampleDark').carousel('cycle');
-        $('#carouselButton').children('span').removeClass('bi-play-fill');
-        $('#carouselButton').children('span').addClass('bi-pause-fill');
-      }          
-    }); 
-    
-  });
-*/
  
